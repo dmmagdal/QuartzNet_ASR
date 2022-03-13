865186631605c41117ec567ff45331effb56f6d0
Diego <dmmagdal@ucsc.edu>
2022-03-13 11:45:39 -0700

    Updated config.py to have save/load function. This will come into play when it comes to saving and loading a previously trained QuartzNet ASR model and initializing it with the correct configuration parameters.

--- config.py
+++ config.py
@@ -11,6 +11,10 @@
 # Windows/MacOS/Linux
 
 
+import os
+import json
+
+
 class Config:
 	def __init__(self, block_repeat=3, module_repeat=5):
 		# Model | block_repeat | module_repeat
@@ -19,3 +23,26 @@ class Config:
 		# 15x5          3              5
 		self.block_repeat = block_repeat
 		self.module_repeat = module_repeat
+
+
+	def save_config(self, path_dir):
+		if not os.path.exists(path_dir):
+			os.makedirs(path_dir, exist_ok=True)
+		with open(os.path.join(path_dir, "config.json"), "w+") as f:
+			data = {
+				"block_repeat": self.block_repeat,
+				"module_repeat": self.module_repeat,
+			}
+			json.dump(data, f, indent=4)
+
+
+	def load_config(self, path_dir):
+		config_file = os.path.join(path_dir, "config.json")
+		if not os.path.exists(path_dir):
+			print(f"Error: Could not locate path: {path_dir}")
+		elif not os.path.exists(config_file):
+			print(f"Error: Could not locate config file: {config_file}")
+		with open(os.path.join(path_dir, "config.json"), "w+") as f:
+			data = json.load(f)
+		self.block_repeat = data["block_repeat"]
+		self.module_repeat = data["module_repeat"]
